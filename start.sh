2f9b5da929ff12128e4b1ca11a2749024b62c187
Aaron Ploetz <aaron.ploetz@datastax.com>
2022-09-15 13:55:07 -0500

    moving CQL files into data/cql/, automating token population

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-java -Dconfig.file=quine.conf -jar quine.jar -r password-spraying-workshop.yml --force-config
+SCBFile=~/.astrarc
+if [ -f "$SCBFile" ]; then
+    # get Astra token
+    token=$(grep ASTRA ~/.astrarc | head -n 1 | cut -f 2 -d =)
+
+    # inject Astra token into quine.conf
+    sed -i 's/ASTRA_TOKEN/$token/' quine.conf
+
+    # start Quine
+    java -Dconfig.file=quine.conf -jar quine.jar -r password-spraying-workshop.yml --force-config
+else
+    echo "Please set up your token with Astra Shell by running:\n astra setup"
+fi

a8549f16c2ba1649c5bbec4dd46ae4528aea9440
Aaron Ploetz <aaron.ploetz@datastax.com>
2022-09-17 11:09:42 -0500

    fixing start so that it properly parses the token and region.

--- start.sh
+++ start.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-astraRC="~/.astrarc"
 dc="workshops"
 
-if [[ -f "$astraRC" ]]; then
+if test -f ~/.astrarc; then
     # get Astra token
     token=$(grep ASTRA ~/.astrarc | head -n 1 | cut -f 2 -d =)
     # inject Astra token into quine.conf
     sed -i "s/ASTRA_TOKEN/\"$token\"/" quine.conf
 
     # get region
-    region=$(astra db list | grep "$dc" | cut -f 4 -d '|' )
+    region=$(astra db list | grep "$dc" | cut -f 4 -d '|')
     # inject Astra token into quine.conf
     sed -i "s/ASTRA_CLOUD_REGION/$region/" quine.conf
 
     # start Quine
     java -Dconfig.file=quine.conf -jar quine.jar -r password-spraying-workshop.yml --force-config
 else
-    echo "Please set up your token with Astra Shell by running:\n astra setup"
+    echo "Please set up your token with Astra Shell by running: astra setup"
 fi

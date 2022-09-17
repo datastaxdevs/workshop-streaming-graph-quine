70cf0f74cd6dde08d468075f0e29b0637796cd8f
Aaron Ploetz <aaron.ploetz@datastax.com>
2022-09-17 10:52:09 -0500

    easing start process by parsing local Astra shell config for token and region

--- start.sh
+++ start.sh
@@ -1,10 +1,18 @@
-SCBFile=~/.astrarc
-if [ -f "$SCBFile" ]; then
+#!/bin/bash
+
+astraRC="~/.astrarc"
+dc="workshops"
+
+if [[ -f "$astraRC" ]]; then
     # get Astra token
     token=$(grep ASTRA ~/.astrarc | head -n 1 | cut -f 2 -d =)
-
     # inject Astra token into quine.conf
-    sed -i 's/ASTRA_TOKEN/$token/' quine.conf
+    sed -i "s/ASTRA_TOKEN/\"$token\"/" quine.conf
+
+    # get region
+    region=$(astra db list | grep "$dc" | cut -f 4 -d '|' )
+    # inject Astra token into quine.conf
+    sed -i "s/ASTRA_CLOUD_REGION/$region/" quine.conf
 
     # start Quine
     java -Dconfig.file=quine.conf -jar quine.jar -r password-spraying-workshop.yml --force-config
